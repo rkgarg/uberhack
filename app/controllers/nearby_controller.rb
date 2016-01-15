3ada23a5872eef89e2f9ec54ddcb0c74c652a22c
rkgarg <rkgarg90@gmail.com>
2016-01-15 16:34:08 +0530

    misc

--- app/controllers/nearby_controller.rb
+++ app/controllers/nearby_controller.rb
@@ -27,9 +27,9 @@ class NearbyController < ApplicationController
 
 
     HTTParty.get("https://developers.zomato.com/api/v2.1/search?lat=28.4774100&lon=77.0689790&radius=20",
-  { 
-    :headers => { 'Accept' => 'application/json', 'user_key' => 'c3fdcb7cd62590868c5de06df7538201'}
-  })
+    { 
+      :headers => { 'Accept' => 'application/json', 'user_key' => 'c3fdcb7cd62590868c5de06df7538201'}
+    })
 
     render :json => final_hash
   end
